d9ce6c0878d5e8f528db346a7b834b2ccd445ccf
mlavin <martylavin1@yahoo.com>
2013-07-02 17:49:22 -0500

    initial commt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreZurb::Application.config.secret_token = 'e1982fe5251ce9959e7f597a9a0277fc3e0a408f6f8de390767e777687d7aeeb63dd9ffb167f0157d283a1f24e92978ab84ab268aca4611fd446079198000fe9'
